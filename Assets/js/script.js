20fcc4dd57a5dbaacadafac3268da684a4e2c703
Aniruddha Bhattacharya <p.aniruddhabhattacharya@gmail.com>
2021-04-25 14:21:44 -0700

    added auditSchedule, which color codes the schedule | fixed error caused by local storage and saveSchedule

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,5 @@
-var dailySchedule = [];
+var dailySchedule =JSON.parse(localStorage.getItem("days-schedule"))||[]; // Calling this as a global value to avoid dailySchedule array becoming undefined which causes errors when inputting and saving values
+//var dailySchedule = [];
 var currentDate = moment();
 var currentDay = function() {
     var currentDayEl = $("#currentDay")[0];
@@ -6,7 +7,7 @@ var currentDay = function() {
     var daySchedule = $("#daySchedule")[0];
     var taskList = $("<ul>").appendTo(daySchedule);
     for (var i = 0; i < 24; i++){
-        createSchedule(taskList, i)
+        createSchedule(taskList, i);
     }
 };
 
@@ -18,20 +19,20 @@ var createSchedule = function(taskList, i) {
         .appendTo(taskList);
 
 
-    var hourEl = $("<div>")
+    var hourEl = $("<span>")
         .addClass("col-1")
         .addClass("hour")
         .appendTo(taskLi);
 
     hourEl[0].innerHTML = moment({hour: i}).format("h A");
 
-    var taskEl = $("<textarea>")
+    var textEl = $("<textarea>")
         .addClass("col-10")
         .addClass()
         .attr("id", "event-"+ i);
 
-    taskEl[0].innerText = "";
-    taskLi.append(taskEl);
+    textEl[0].innerText = "";
+    taskLi.append(textEl);
 
     var saveBtnEl = $("<button>")
         .addClass("col-1 saveBtn oi oi-cloud-upload")
@@ -39,19 +40,19 @@ var createSchedule = function(taskList, i) {
         .click(saveSchedule)
         .appendTo(taskLi);
     
-    loadSchedule(taskEl, i);
+    loadSchedule(textEl, i);
+    auditSchedule(textEl, i);
 
 };
 
-var loadSchedule = function(taskEl, i) {
-    dailySchedule = JSON.parse(localStorage.getItem("days-schedule"));
-    console.log(dailySchedule);
+var loadSchedule = function(taskText, i) {
+   /* dailySchedule = JSON.parse(localStorage.getItem("days-schedule"));
     if(!dailySchedule) {
         dailySchedule = [];
-    }
+    }*/ // I have to parse This globally otherwise it will pass in a null or undefined value, causing errors when trying to save on some cells.
 
     if(dailySchedule[i]){
-        taskEl[0].innerText = dailySchedule[i].description;
+        taskText[0].innerText = dailySchedule[i].description;
     }
     else {
 
@@ -65,16 +66,27 @@ var loadSchedule = function(taskEl, i) {
 };
 
 var saveSchedule = function(event){
+    console.log("Event was clicked: ", event);
     var blockHour = this.getAttribute("block-hr-id");
+    console.log("blockHour = ", blockHour);
     var blockText = $("#event-"+blockHour).val();
-
+    console.log("blockText = ", blockText);
+    console.log(dailySchedule[blockHour]);
     dailySchedule[blockHour].time = parseInt(blockHour);
     dailySchedule[blockHour].description = blockText;
     localStorage.setItem('days-schedule', JSON.stringify(dailySchedule));
 };
 
-var auditSchedule = function(){
-
+var auditSchedule = function(taskText, iHour){
+    if(moment(currentDate).format("H") > iHour){
+        taskText.addClass("past");
+    }
+    else if(moment(currentDate).format("h A") === moment({hour: iHour}).format("h A")){
+        taskText.addClass("present");
+    }
+    else {
+        taskText.addClass("future");
+    }
 };
 
 
